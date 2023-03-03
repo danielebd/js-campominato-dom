7eb3c123378f420694c4d66a92a7a1ae93541d23
danielebd <danielebalebalestro@gmail.com>
2023-03-03 17:28:11 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+'use strict';
+
+function myCreateElement(myCase, htmlElement, myClass, text) {
+    const container = document.querySelector(myCase);
+    const element = document.createElement(htmlElement);
+
+    element.classList.add(myClass);
+    container.append(element);
+    element.append(text);
+
+    return element;
+}
+
+function numberCell(numCell, nameClass) {
+    let container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    for (let i = 1; i <= numCell; i++) {
+        myCreateElement('.container', 'div', nameClass, i);
+    }
+
+    let inputCell = document.querySelectorAll(`.${nameClass}`);
+    inputCell.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            elem.classList.add('cell-click');
+            console.log(elem.innerHTML)
+        })
+    })
+}
+
+const inputButton = document.querySelector('.play');
+inputButton.addEventListener('click', function () {
+
+    const inputChallenge = document.querySelector('#challenge').value;
+    let range = '';
+    console.log(inputChallenge);
+    if (inputChallenge === 'easy') {
+        range = 100;
+        numberCell(range, 'cell-100');
+    }
+    else if (inputChallenge === 'normal') {
+        range = 81;
+        numberCell(range, 'cell-91');
+    }
+    else {
+        range = 49;
+        numberCell(range, 'cell-49');
+    }
+})
+
+
+
